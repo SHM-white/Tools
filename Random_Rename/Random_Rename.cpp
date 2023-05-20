bfb80c15f19c62ac16ae7fdddb1b2ab4bf5f512d
White SHM <1131287310a@gmail.com>
2023-05-20 18:50:32 +0800

    初始化

---
+++ Random_Rename/Random_Rename.cpp
@@ -0,0 +1,52 @@
+﻿// Random_Rename.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <io.h>
+#include <iostream>
+#include <vector>
+std::vector<std::string> getFileNames(std::string path = ".\\");
+int main()
+{
+    std::cout << "start\n" << std::endl;
+    std::cout << "Reading file names....\n" << std::endl;
+	std::vector<std::string> fileNames = getFileNames();
+	for (auto name : fileNames) {
+		std::cout << name << "\n";
+	}
+	std::cout << std::endl;
+}
+
+std::vector<std::string> getFileNames(std::string path) {
+	std::vector<std::string> files;
+	intptr_t hFile = 0;
+	struct _finddata_t fileinfo;
+	std::string p;
+	if ((hFile = _findfirst(p.assign(path).append("\\*").c_str(), &fileinfo)) != -1)
+	{
+		do
+		{
+			//如果是目录,递归查找
+			//如果不是,把文件绝对路径存入vector中
+			if ((fileinfo.attrib & _A_SUBDIR))
+			{
+				//if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0)
+					//getFileNames(p.assign(path).append("\\").append(fileinfo.name), files);
+			}
+			else
+			{
+				files.push_back(p.assign(path).append(fileinfo.name));
+			}
+		} while (_findnext(hFile, &fileinfo) == 0);
+		_findclose(hFile);
+	}
+	return files;
+}
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
